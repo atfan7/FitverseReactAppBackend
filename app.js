490460f0e33e40b5ed8b65a59b24a017123d2a29
Mohd Atfan <143632074+atfan7@users.noreply.github.com>
2025-10-22 01:22:30 +0530

    Refactor app.js for improved structure and routing

--- app.js
+++ app.js
@@ -1,38 +1,31 @@
+
 const express = require("express");
-
-const cors = require("cors");
-const app = express();
-
-
-app.use(cors({
-  origin: "https://fitverse-project-by-atfan.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE"],              
-  credentials: true
-}));
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Server is running ✅");
-});
-
 const classRouting = require("./router/classRouting");
+require("./dbconfig/dbconfig")
+const app=express();
+const cors= require("cors");
 const trainerRouting = require("./router/trainerRouting");
 const scheduleRouting = require('./router/scheduleRouting');
 const enquiryRouting = require("./router/enquiryRouting");
 const classDetailsRouting = require("./router/classDetailsRouting");
 const registerRouter = require("./router/registerRouting");
 const contactRouting = require("./router/contactRouting");
+app.use(cors());
 
-app.use("/", classRouting);
-app.use("/", trainerRouting);
-app.use("/", scheduleRouting);
+const port=4000;
+app.use(express.json());
+
+
+app.use("/",classRouting);
+app.use("/",trainerRouting);
+app.use("/",scheduleRouting);
 app.use("/", enquiryRouting);
 app.use("/", classDetailsRouting);
-app.use("/", registerRouter);
-app.use("/", contactRouting);
+app.use("/",registerRouter);
+app.use("/",contactRouting);
 
 
-const port = process.env.PORT || 4000;
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+app.listen(port,()=>{
+
+    console.log(`Server started at ${port}`)
+})
